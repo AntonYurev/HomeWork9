6950c0173165361cff4cbd18ea462bf56496a61c
Anton <altay77@inbox.ru>
2022-10-02 11:51:32 +0700

    Task9_2 solution

---
+++ Task9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+void Sum()
+    {
+        int sum=0;
+        for (int i=m; i<=n;i++)
+            {
+                sum = sum + i;
+            }
+        Console.Write($"Сумма чисел от {m} до {n} равна {sum}");
+    }
+if (m>n) 
+    {
+       int a=m; m=n; n=a; 
+    }
+Sum();
