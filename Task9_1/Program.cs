e353da38da9c63820482d5c4f33dcb88c682ed3d
Anton <altay77@inbox.ru>
2022-10-02 11:26:47 +0700

    Task9_1 solution

---
+++ Task9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от M до N.
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+void Print()
+            {
+            for (int i = m; i <= n; i++)
+                {
+                    Console.Write(i + "\t" );
+                }
+            }
+if (m>n) 
+    {
+       int a=m; m=n; n=a; 
+    }
+Print();
